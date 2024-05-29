53897f0c93a4742b9f9bf1dcdb87a2cd7904047e
Csizmadia Márk <airamek@teamorange.hu>
2024-05-29 12:01:07 +0200

    asd

--- SZGYA-WPF-HasznaltAuto/Car.cs
+++ SZGYA-WPF-HasznaltAuto/Car.cs
@@ -1,31 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace SZGYA_WPF_HasznaltAuto
 {
     enum Fuel
     {
-        PETROL = 0,
-        DIESEL = 1,
-        ELECTRIC = 2
+        [Description("Mindegy")]
+        ALL = 0,
+        [Description("Benzin")]
+        PETROL,
+        [Description("Dízel")]
+        DIESEL,
+        [Description("Elektromos")]
+        ELECTRIC 
     }
 
     enum Form
     {
-        CABRIO = 0,
-        OFF_ROAD = 1,
-        COUPE = 2,
-        HATCHBACK = 3,
-        WAGON = 4
+        [Description("Mindegy")]
+        ALL = 0,
+        [Description("Kabrió")]
+        CABRIO,
+        [Description("Terepjáró")]
+        OFF_ROAD,
+        [Description("Coupe")]
+        COUPE,
+        [Description("Ferdehátú")]
+        HATCHBACK,
+        [Description("Kombi")]
+        WAGON,
     }
+    
 
     internal class Car
     {
         public string Brand { get; set; }
         public string Model { get; set; }
+
+        public string Name { get; set; }
+
         public Form FormFactor { get; set; }
         public Fuel FuelType { get; set; }
         public int ReleaseYear { get; set; }
